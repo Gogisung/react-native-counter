2fc036238641c12853ee7b1d9fdfc4b7f0776255
Gogisung <ggs0707@naver.com>
2020-02-19 00:25:22 +0900

    screen counter 작성중

--- src/Screens/Counter/index.tsx
+++ src/Screens/Counter/index.tsx
@@ -16,4 +16,38 @@ const TitleLabel = Styled.Text`
   font-size: 24px;
 `;
 
+const CountContainer = Styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
 
+const CountLabel = Styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const ButtonContainer = Styled.View`
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+interface Props {
+  title?: string;
+  initValue: number;
+}
+
+const Counter = ({ title, initValue }: Props) => {
+  const [ count, setCount ] = useState<number>(0);
+  return (
+    <Coutainer>
+      <CountContainer>
+
+      </CountContainer>
+    </Coutainer>
+  )
+};
+
+export default Counter;
