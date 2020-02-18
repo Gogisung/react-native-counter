48a078b6672b7c9e850c27ce788b2cc642c49351
Gogisung <ggs0707@naver.com>
2020-02-19 00:12:24 +0900

    README 만들기

--- src/Screens/Counter/index.tsx
+++ src/Screens/Counter/index.tsx
@@ -1,1 +1,19 @@
-import React, {useState}
+import React, { useState } from 'react';
+import Styled from 'styled-components/native';
+import Button from '~/Components/Button';
+
+const Container = Styled.SafeAreaView`
+  flex: 1;
+`;
+
+const TitleContainer = Styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleLabel = Styled.Text`
+  font-size: 24px;
+`;
+
+
